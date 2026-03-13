16867e0eb2fb9ba363c84655c9cab2d5717999f8
nsdaveta <100615660+nsdaveta@users.noreply.github.com>
2026-03-13 14:16:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,5 +14,5 @@ app.use('/todos',TodoRouter)
 const PORT = process.env.PORT || 4000;
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/todos';
 
- mongoose.connect(MONGO_URI).then(()=>console.log("Connected To Database"));
+ mongoose.connect(MONGO_URI, {tls : true}).then(() => console.log("Connected To Database"));
  app.listen(PORT, () => {console.log(`Server is listening on port ${PORT}`);});
